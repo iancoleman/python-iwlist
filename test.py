808caf777fd08d92bc234ef4f9abd1c44990b667
Ian Coleman <coleman.ian@gmail.com>
2017-06-16 14:40:02 +1000

    Run tests for multiple different scans

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import json
+import os
 import iwlist
 import unittest
 
@@ -11,25 +12,38 @@ def fileContent(filename):
 class TestParse(unittest.TestCase):
 
     def setUp(self):
-        self.parsed = iwlist.parse(fileContent("test/scan.txt"))
-        self.expected = json.loads(fileContent("test/vectors.json"))
+        dirs = os.listdir("test")
+        self.cases = []
+        for d in dirs:
+            scanFile = os.path.join("test", d, "scan.txt")
+            vectorsFile = os.path.join("test", d, "vectors.json")
+            case = {
+                    "name": d,
+                    "parsed": iwlist.parse(fileContent(scanFile)),
+                    "expected": json.loads(fileContent(vectorsFile)),
+                }
+            self.cases.append(case)
 
     def test_parse_length(self):
-        self.assertEqual(len(self.expected), len(self.parsed))
+        for case in self.cases:
+            self.assertEqual(len(case["expected"]), len(case["parsed"]))
 
     def test_cells_have_all_expected_keys(self):
-        for i in range(len(self.expected)):
-            e = self.expected[i]
-            p = self.parsed[i]
-            for key in e:
-                self.assertTrue(key in p)
+        for case in self.cases:
+            for i in range(len(case["expected"])):
+                e = case["expected"][i]
+                p = case["parsed"][i]
+                for key in e:
+                    msg = "key missing: %s\n%s\ncellnumber: %s" % (key, case["name"], e["cellnumber"])
+                    self.assertTrue(key in p, msg)
 
     def test_cells_dont_have_extra_keys(self):
-        for i in range(len(self.expected)):
-            e = self.expected[i]
-            p = self.parsed[i]
-            for key in p:
-                self.assertTrue(key in e)
+        for case in self.cases:
+            for i in range(len(case["expected"])):
+                e = case["expected"][i]
+                p = case["parsed"][i]
+                for key in p:
+                    self.assertTrue(key in e)
 
 if __name__ == '__main__':
     unittest.main()
